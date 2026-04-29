94a43adb1bd3e4a17ab12ef6bdcf80497c665985
oliverhessler76-glitch <oliver.hessler.76@googlemail.com>
2026-04-29 08:43:04 +0200

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,1 +1,12 @@
-
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open('watzmann').then(cache => {
+      return cache.addAll([
+        '/',
+        '/index.html',
+        '/style.css',
+        '/app.js'
+      ]);
+    })
+  );
+});
